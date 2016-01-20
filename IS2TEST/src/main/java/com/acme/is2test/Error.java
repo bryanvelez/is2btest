b35d4a214ff5a4edb4c8594ac98e97836850e729
PC16 <PC16@PC16>
2016-01-20 18:27:49 -0500

    Commit nuebo branch

---
+++ IS2TEST/src/main/java/com/acme/is2test/Error.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acme.is2test;
+
+/**
+ *
+ * @author PC16
+ */
+public class Error {
+    djjmggmhjhj,j,hkj,ktu,iy,u,i,jj
+    
+}
